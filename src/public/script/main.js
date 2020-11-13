86741e57f5065b76a3f38e6cba4b3fee0d5f791d
luisricardo123 <58831965+luisricardo123@users.noreply.github.com>
2020-11-13 00:23:09 -0500

    Agregado la descripcion del evento

--- src/public/script/main.js
+++ src/public/script/main.js
@@ -1,3 +1,4 @@
+//Evento para que el boton menu se despliegue
 document.querySelector(".btn-menu").addEventListener('click', (e) => {
     document.querySelector(".nav").classList.toggle("show")
 })
